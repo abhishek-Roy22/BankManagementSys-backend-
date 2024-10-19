7bd0713015ee2443ae9d1026ee7586d58996acd3
abhishek-Roy22 <100830257+abhishek-Roy22@users.noreply.github.com>
2024-10-19 13:18:47 +0530

    cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const URL = process.env.MONGO_URI;
 // Middleware
 app.use(
   cors({
-    origin: 'https://bankmanagementsys.netlify.app', // Netlify frontend URL
+    origin: '*', // Netlify frontend URL
     credentials: true, // if you need to send cookies
   })
 );
